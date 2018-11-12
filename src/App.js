b15be0befc439e04ccabd03cd448d3b37c8ab579
abby <abby.greenbloom@gmail.com>
2018-11-12 13:17:22 -0500

    initial integration of api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      gifs: [],
+      store: []
+    }
+  }
+
+
+  componentDidMount() {
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=Mb6qpsLotHxYjpxVHGS9a1WWt0nRcepJ&rating=g')
+    .then(resp => console.log(resp.data.data))
+  }
+
   render() {
     return (
       <div className="App">
